c30507bfb0eaa1797e6b5d7a2e16310fb4e958fa
Adel Lassag <adetech.aadl@gmail.com>
2018-08-27 21:37:29 +0100

    feat: Import Link tag from router dom and replace a tag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Book from './book'
 
 class Main extends Component {
@@ -49,7 +50,7 @@ class Main extends Component {
             </div>
         </div>
         <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            <Link to="/search" >Add a book</Link>
         </div>
         </div>
     )
--- src/search.js
+++ src/search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './book'
 
@@ -35,7 +36,7 @@ class Search extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className="close-search" to="/">Close</Link>
                     <div className="search-books-input-wrapper">
                     {/*
                         NOTES: The search from BooksAPI is limited to a particular set of search terms.
