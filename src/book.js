589c1b6fa1ea59131f3a06b3a64bb819e7a4158b
Adel Lassag <adetech.aadl@gmail.com>
2018-08-27 20:20:03 +0100

    fix: Fix image thumbnail issue

--- src/book.js
+++ src/book.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 class Book extends Component {
   render() {
+    let cover = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : 'https://as.nyu.edu/content/dam/nyu-as/faculty/images/PlaceholderBook.png'
     return (
         <div className="book">
             <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${this.props.book.imageLinks.thumbnail}')` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${cover}')` }}></div>
             <div className="book-shelf-changer">
                 <select
                     onChange={(event)=> this.props.changeShelf(this.props.book, event.target.value)}
