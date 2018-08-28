ccf0e2661022f49ad1fef440319e5b5f13a64ef7
Adel Lassag <adetech.aadl@gmail.com>
2018-08-29 00:16:27 +0100

    docs: comments part IV

--- src/search.js
+++ src/search.js
@@ -4,34 +4,37 @@ import * as BooksAPI from './BooksAPI'
 import Book from './book'
 
 class Search extends Component {
+    /* Create state that contain empty string for search query,
+     * also create an empty array to store fetched books from BooksApi
+     */
     state = {
         query : '',
         searchBooks : []
     }
-
+    // Create a methode that update query state
     updateQuery = (query) => {
         this.setState({
             query : query
         })
         this.getBooks(query)
     }
-
+    // Create a methode that fetch books from search query
     getBooks = (query) => {
-        if (query) {
-            BooksAPI.search(query).then((searchBooks)=>{
-                if (searchBooks.error) {
-                    this.setState({searchBooks : []})        
+        if (query) {// If there is a query
+            BooksAPI.search(query).then((searchBooks)=>{// When search promise is resolved
+                if (searchBooks.error) {// if the query result doesn't match any book fetched
+                    this.setState({searchBooks : []}) // return an emty array and throw error
                 }
-                else {
-                    this.setState({searchBooks})
+                else {// If it matches
+                    this.setState({searchBooks})// Push fetched book to that array
                 }
             })
         }
         else {
-            this.setState({searchBooks : []})
+            this.setState({searchBooks : []})// Else return an emty array
         }
     }
-
+    // Render UI
     render() {
         return (
             <div className="search-books">
@@ -45,7 +48,10 @@ class Search extends Component {
 
                         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                         you don't find a specific author or title. Every search is limited by search terms.
-                    */}
+                    */
+                   
+                    // TODO: Autofocus and handle update query method on change event
+                   }
                     <input type="text" autoFocus placeholder="Search by title or author" value = {this.state.query} onChange = {(event)=>{this.updateQuery(event.target.value)}}/>
 
                     </div>
@@ -53,7 +59,9 @@ class Search extends Component {
                 <div className="search-books-results">
                     <ol className="books-grid">
                         
-                        {
+                        { /*TODO: Map over each book and check if there is one that is shelfed, get it's shelf value
+                           * if not, then set it's shelf value to 'none'
+                           */
                             this.state.searchBooks.map(bookSearched =>{
                                 bookSearched.shelf = 'none';
                                 this.props.books.map(book =>{
